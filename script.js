fd0b68ec831759c9fb887a762219d5865712a0df
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-04-25 18:17:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
  function calculateArea() {
             // Get the radius from the user using the prompt() function
-            let radius = prompt("Enter the radius of the circle:");
-            // write you code here and display the result to the user
+          
+	 let radius = prompt("Enter the radius of the circle:");
+    radius = parseFloat(radius);
 
-	 let area = 3.14*radius*radius;
-	 return area;
+    // Calculate area if input is valid
+    if (radius > 0) {
+        let area = Math.PI * radius * radius;
+        area = area.toFixed(2);
+        console.log(`The area of the circle with radius ${radius} is ${area}`);
+        return area;
+    } else {
+        console.log("Please enter a positive number for radius.");
+        return null;
+    }
+	 
            
 }
 calculateArea();
